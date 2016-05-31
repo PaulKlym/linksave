055bc36182b3f73297313d94402b84beb7d29735
paul <paul@example.org>
2016-05-31 11:58:54 +0300

    Added enty point for openshift

---
+++ app.py
@@ -0,0 +1,10 @@
+import imp
+import os
+
+if __name__ == '__main__':
+       ip   = os.environ['OPENSHIFT_PYTHON_IP']
+       port = int(os.environ['OPENSHIFT_PYTHON_PORT'])
+       app = imp.load_source('app', 'links.py')
+
+       app.application.listen(port , ip)
+       app.ioloop.IOLoop.instance().start()
